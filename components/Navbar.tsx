a8f43522578db460ab3cc638154de62e6c920921
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-30 10:48:10 +0800

    Implemented Google, Apple and email auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,21 @@
 'use client';
 import React from 'react';
+import Link from 'next/link';
 
 import { useScrollTop } from '@/hooks/use-scroll-top';
 import { cn } from '@/lib/utils';
 
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+
 import Logo from './Logo';
 import ThemeSwitch from './ThemeSwitch';
+import { Button, buttonVariants } from './ui/button';
+import Spinner from './Spinner';
 
 export default function Navbar() {
-  const scrolled = useScrollTop({ threshold: 10 });
+  const scrolled = useScrollTop({ threshold: 10 }),
+    { isAuthenticated, isLoading } = useConvexAuth();
 
   return (
     <div
@@ -18,8 +25,35 @@ export default function Navbar() {
       )}
     >
       <Logo />
-      <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
-        <div>Login</div>
+      <div className='ml-auto justify-end w-full flex items-center gap-x-2'>
+        {isLoading && (
+          <div className='hidden sm:block'>
+            <Spinner />
+          </div>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <div className='hidden sm:flex flex-row gap-x-2'>
+            <div className={buttonVariants({ size: 'sm', variant: 'outline' })}>
+              <SignInButton />
+            </div>
+            <div className={buttonVariants({ size: 'sm' })}>
+              <SignUpButton />
+            </div>
+          </div>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Link href='/'>
+              <Button
+                variant='ghost'
+                size='sm'
+              >
+                Enter Synapse
+              </Button>
+            </Link>
+            <UserButton />
+          </>
+        )}
         <ThemeSwitch />
       </div>
     </div>
