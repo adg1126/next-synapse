8c2418409a19479290887e7238f8da3cf981cdf6
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-30 11:25:37 +0800

    Conditionally render button based on user auth

--- app/(root)/_components/Heading.tsx
+++ app/(root)/_components/Heading.tsx
@@ -2,9 +2,15 @@
 import React from 'react';
 
 import { Button } from '@/components/ui/button';
+import Spinner from '@/components/Spinner';
 import { ArrowRight } from 'lucide-react';
 
+import { useConvexAuth } from 'convex/react';
+import { SignInButton } from '@clerk/nextjs';
+
 export default function Heading() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
@@ -15,10 +21,24 @@ export default function Heading() {
         Synapse is the connected connected workspace where <br />
         better and faster work happens.
       </h3>
-      <Button>
-        Enter Synapse
-        <ArrowRight className='h-4 w-4 ml-2' />
-      </Button>
+      {isLoading && (
+        <div className='w-full flex items-center justify-center'>
+          <Spinner size='lg' />
+        </div>
+      )}
+      {isAuthenticated && !isLoading ? (
+        <Button>
+          Enter Synapse
+          <ArrowRight className='h-4 w-4 ml-2' />
+        </Button>
+      ) : (
+        <SignInButton>
+          <Button size='lg'>
+            Get Started
+            <ArrowRight className='h-4 w-4 ml-2' />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 }
