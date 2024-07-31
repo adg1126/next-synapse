7e79069071928bc1f21335916d4b404e2fa4f152
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-31 10:52:36 +0800

    Added Toaster for displaying notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+
 import { ThemeProvider } from '@/providers/theme-provider';
 import { ConvexClientProvider } from '@/providers/convex-provider';
 import StoreProvider from '@/providers/store-provider';
+
+import { Toaster } from 'sonner';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -63,6 +66,7 @@ export default function RootLayout({
               disableTransitionOnChange
               storageKey='synapse-theme'
             >
+              <Toaster position='bottom-center' />
               {children}
             </ThemeProvider>
           </StoreProvider>
