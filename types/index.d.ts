5535d636c2f2fc0b12ddaaa99ec898d0ca0ce4e8
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-31 17:47:57 +0800

    Updated

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,35 @@
+import { Id } from '@/convex/_generated/dataModel';
+import { LucideIcon } from 'lucide-react';
+
 declare interface ConvexClientProviderProps {
   children: React.ReactNode;
 }
 
 declare interface NewNoteButtonProps {
   label?: String;
-  hintLabel?: String;
   className?: string;
   icon?: React.ReactNode;
+}
+
+declare interface UserItemProps {
+  id?: Id<'documents'>;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpanded?: () => void;
+  label: String;
+  icon: LucideIcon;
+  onClick: () => void;
+}
+
+declare interface TooltipProps {
+  label: string;
+  subtitle?: string;
+  children: React.ReactNode;
+  side?: 'top' | 'bottom' | 'left' | 'right';
+  align?: 'start' | 'center' | 'end';
+  sideOffset?: number;
+  alignOffset?: number;
 }
